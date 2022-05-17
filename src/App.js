6ed92f9222edc6d26aaf2441a17e0419eace9831
Alyssa Falcione <lyzarddz@gmail.com>
2022-05-17 13:27:51 -0400

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import { Button } from '@material-ui/core';
+
 import { makeStyles } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './Components/NavBar';
-
-
+import HomePage from './Components/HomePage';
+import Signup from './Components/Signup';
+import Login from './Components/Login';
 
 const useStyles = makeStyles({
   buttonColor:{
     backgroundColor: 'red'
   }
 })
-
+ 
 function App() {
   const classes = useStyles();
 
   return (
-    <div >
+    <Router>
       <NavBar/>
-      <h1>Hello World</h1>
-      <Button variant="contained" color="primary" className= {classes.buttonColor}>Goodbye World</Button>
-    </div> 
+      <Routes>
+      <Route to="/" element= {<HomePage />} />
+      <Route to="/signup" element= {<Signup />} />
+      <Route to="/login" element= {<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
- 
+  
