fb2a322f0b83c288ad5474a73b7796681312552a
Alyssa Falcione <lyzarddz@gmail.com>
2022-05-26 00:06:48 -0400

    added state to login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
 import { GiCarnivorousPlant} from "react-icons/gi";
 
 const Login = () => {
+
+  const [username, setUsername] = useState("");
+  const [users, setUsers] = useState([])
+
+  function handleChange(e){
+    setUsername(e.target.value);
+  }
+
+  function handleSubmit(e){
+   e.preventDefault();
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:3001/users')
+    .then(resp => resp.json())
+    .then(data => setUsers(data))
+
+
+  }, [])
+
+
   return (
     <div>
-    <h1> <GiCarnivorousPlant/> Login <GiCarnivorousPlant/> </h1>
+    <h1> <GiCarnivorousPlant/> Login <GiCarnivorousPlant/> </h1>   
     <form>
       <div>
         <label htmlFor="username">Username: </label>
-        <input type="text" name="username" id="username"></input>
+        <input type="text" name="username" id="username" onChange={handleChange} value={username}></input>
       </div>
-      <input type="submit" value="Login" />
+      <br></br>
+      <input type="submit" value="Login" onClick={handleSubmit}/>
     </form>
     </div>
     
